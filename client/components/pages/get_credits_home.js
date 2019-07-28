9877f338230bc7c9ab70e12babf203a545ffdd65
Valentin Munrdov <Valent@Georgis-MacBook-Air.local>
2019-07-28 13:38:48 +0300

    late july2019 update

--- client/components/pages/get_credits_home.js
+++ client/components/pages/get_credits_home.js
@@ -7,13 +7,19 @@ class CreditsHome extends React.Component {
   render() {
     const lan = {
       es: ['Jugar por Créditos! Si - asi de simple. Solo juegue y gane... fasilisimo.',
-        'Pon Dinero: comprar creditos es doloroso, lo sé... pero da mucho más!! Pagar por Créditos, adrinalina su Latido (hasta poner el color de su corazón en dorado) y sube el estrato de su cuenta. Además, a nostras - las chicas, nos gusta el efectivo. Yo le devolveré el favor, no dudes!',
-        'Pon datos: es la manera menos dolorosa: averigüe si su perfil todavía tiene datos por llenar. Solo haz un Click...',
+        'Pon Dinero: comprar creditos es doloroso, lo sé... pero da mucho más!! Pagar por Créditos, \
+        adrinalina su Latido (hasta poner el color de su corazón en dorado) y sube el estrato de su cuenta. \
+        Además, a nostras - las chicas, nos gusta el efectivo. Yo le devolveré el favor, no dudes!',
+        'Pon datos: es la manera menos dolorosa: averigüe si su perfil todavía tiene datos por llenar. \
+        Solo haz un Click...',
         'Por Más Créditos:', 'Jugar por Créditos', 'Comprar Créditos', 'Datos por Créditos'
       ],
       en: ['Play a Game! Yeah, simple as that - just click arround and earn some easy coins for fun...',
-        'Put some Cash: this is painfull, I know, BUT - it gives you so much more! It elevates your HeartBeats, too (till the color of your heart turns golden-yellow) and makes you special, you\'ll notice... and so will I. Plus: we, girls, love cash (among other things) and I\'ll make it up to you!!!',
-        'Put some Data: this is so far the painless way - check if your profile still has left some missing data to exchange for credits. Just Click...',
+        'Put some Cash: this is painfull, I know, BUT - it gives you so much more! It elevates your \
+        HeartBeats, too (till the color of your heart turns golden-yellow) and makes you special, you\'ll \
+        notice... and so will I. Plus: we, girls, love cash (among other things) and I\'ll make it up to you!!!',
+        'Put some Data: this is so far the painless way - check if your profile still has left \
+        some missing data to exchange for credits. Just Click...',
         'To Get Credits:', 'Play for Credits', 'Buy Credits', 'Data for Credits'
       ]
     }
